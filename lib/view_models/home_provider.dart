8d6a12a37ce3e8333a5f14af76bb576ebee8e38e
Bahaa Fathi <bahaafathi@outlook.sa>
2021-03-23 13:28:33 -0400

    inti

---
+++ lib/view_models/home_provider.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ebook_app/models/category.dart';
+import 'package:flutter_ebook_app/util/api.dart';
+import 'package:flutter_ebook_app/util/enum/api_request_status.dart';
+import 'package:flutter_ebook_app/util/functions.dart';
+
+class HomeProvider with ChangeNotifier {
+  CategoryFeed top = CategoryFeed();
+  CategoryFeed recent = CategoryFeed();
+  APIRequestStatus apiRequestStatus =
+      APIRequestStatus.loading; //اي نام حاطت فيه اسامي للتحميل
+  Api api = Api();
+//TODO:Learn Try and Catch
+  getFeeds() async {
+    setApiRequestStatus(APIRequestStatus.loading);
+    try {
+      CategoryFeed popular = await api.getCategory(Api.popular);
+      setTop(popular);
+      CategoryFeed newReleases = await api.getCategory(Api.recent);
+      setRecent(newReleases);
+      setApiRequestStatus(APIRequestStatus.loaded);
+    } catch (e) {
+      checkError(e);
+    }
+  }
+
+//اول لما يعمل جيت فيد
+//هيعبي المتغيرات الي فوق في الكلاس دي الي هي
+//التوب والريسينت
+  ///من الاخر لازم يعمل
+  ///جيت فيد الاول قبل اي حاجه
+  ///بعد يعمل جيت فيد يقدر بعد كدة
+  ///يستخدم المتغيرات الي فوق او يستخدم الفانكشن الجيتر
+  ///بتاعت الريسينت والتوب
+
+  CategoryFeed getRecent() {
+    return recent;
+  }
+
+  CategoryFeed getTop() {
+    return top;
+  }
+
+  void checkError(e) {
+    if (Functions.checkConnectionError(e)) {
+      setApiRequestStatus(APIRequestStatus.connectionError);
+    } else {
+      setApiRequestStatus(APIRequestStatus.error);
+    }
+  }
+
+  void setApiRequestStatus(APIRequestStatus value) {
+    apiRequestStatus = value;
+    notifyListeners();
+  }
+
+  void setTop(value) {
+    top = value;
+    notifyListeners();
+  }
+
+  void setRecent(value) {
+    recent = value;
+    notifyListeners();
+  }
+}
---
+++ lib/views/home/home.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_ebook_app/components/body_builder.dart';
+import 'package:flutter_ebook_app/components/book_card.dart';
+import 'package:flutter_ebook_app/components/book_list_item.dart';
+import 'package:flutter_ebook_app/models/category.dart';
+import 'package:flutter_ebook_app/util/consts.dart';
+import 'package:flutter_ebook_app/util/router.dart';
+import 'package:flutter_ebook_app/view_models/home_provider.dart';
+import 'package:flutter_ebook_app/views/genre/genre.dart';
+import 'package:provider/provider.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+///AutomaticKeepAliveClientMixin
+///عشان يخلي الصفحه لسه معاها الداتا بتعتها
+///حتي لو قفل الصفحه ورجع فتحها تاني
+class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin {
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback(
+      //called only once after Build widgets done with rendering
+      (_) => Provider.of<HomeProvider>(context, listen: false).getFeeds(),
+
+      ///بيعبي المتغيرات
+      ///tob,recent
+      ///بعد لما البكسلات تترسم
+      ///يعني الحاجات دي ناال اصلا والبكسلات بتترسم
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context); //AutomaticKeepAliveClientMixin
+    return Consumer<HomeProvider>(
+      builder: (BuildContext context, HomeProvider homeProvider, Widget child) {
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              '${Constants.appName}',
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+          body: _buildBody(homeProvider),
+        );
+      },
+    );
+  }
+
+//عمل هنا حركه جامدة جدا
+  ///عمل ويديجت بتشيك علي المتغير بتاع التحميل
+  ///وبيشوفه لو بيحمل يعرض ويديت التحميل
+  ///لو في ايرور يعرض ويديجت الايرور
+  ///لو كلو تمام يعرض عادي الويدجيت بتاعت التطبيق
+  ///
+  Widget _buildBody(HomeProvider homeProvider) {
+    return BodyBuilder(
+      apiRequestStatus: homeProvider.apiRequestStatus,
+      child: _buildBodyList(homeProvider),
+      reload: () => homeProvider.getFeeds(),
+    );
+  }
+
+  Widget _buildBodyList(HomeProvider homeProvider) {
+    return RefreshIndicator(
+      onRefresh: () => homeProvider.getFeeds(), //بيعبي المتغيرات لما يعمل ريفرش
+      child: ListView(
+        children: <Widget>[
+          _buildFeaturedSection(homeProvider),
+          SizedBox(height: 20.0),
+          _buildSectionTitle('Categories'),
+          SizedBox(height: 10.0),
+          _buildGenreSection(homeProvider),
+          SizedBox(height: 20.0),
+          _buildSectionTitle('Recently Added'),
+          SizedBox(height: 20.0),
+          _buildNewSection(homeProvider),
+        ],
+      ),
+    );
+  }
+
+  _buildSectionTitle(String title) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            '$title',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildFeaturedSection(HomeProvider homeProvider) {
+    ///Here Man Your Are Asume
+    return Container(
+      height: 200.0,
+      child: Center(
+        child: ListView.builder(
+          primary: false,
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          scrollDirection: Axis.horizontal,
+          itemCount: homeProvider?.top?.feed?.entry?.length ??//هنا خد بالك المتغيرات فاضيه
+              0, //todo: Learn ? and ?? in dart
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) {
+            Entry entry = homeProvider.top.feed.entry[index];
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+              child: BookCard(
+                img: entry.link[1].href,
+                entry: entry,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  _buildGenreSection(HomeProvider homeProvider) {
+    return Container(
+      height: 50.0,
+      child: Center(
+        child: ListView.builder(
+          primary: false,
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          scrollDirection: Axis.horizontal,
+          itemCount: homeProvider?.top?.feed?.link?.length ?? 0,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) {
+            Link link = homeProvider.top.feed.link[index];
+
+            // We don't need the tags from 0-9 because
+            // they are not categories
+            if (index < 10) {
+              return SizedBox();
+            }
+
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).accentColor,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                ),
+                child: InkWell(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  onTap: () {
+                    MyRouter.pushPage(
+                      context,
+                      Genre(
+                        title: '${link.title}',
+                        url: link.href,
+                      ),
+                    );
+                  },
+                  child: Center(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        '${link.title}',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  _buildNewSection(HomeProvider homeProvider) {
+    return ListView.builder(
+      primary: false,
+      padding: EdgeInsets.symmetric(horizontal: 15.0),
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: homeProvider?.recent?.feed?.entry?.length ?? 0,
+      itemBuilder: (BuildContext context, int index) {
+        Entry entry = homeProvider.recent.feed.entry[index];
+
+        return Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+          child: BookListItem(
+            img: entry.link[1].href,
+            title: entry.title.t,
+            author: entry.author.name.t,
+            desc: entry.summary.t,
+            entry: entry,
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true; //AutomaticKeepAliveClientMixin
+}
